a48bb5ccf6ba4e6a90d3a72cacc1a99b3f7c811a
Neelam JITENDRA <neelam_jitendra@yahoo.com>
2017-08-12 00:04:09 +0200

    Backend

---
+++ config.sample.js
@@ -0,0 +1,22 @@
+module.exports = {
+  twitter: {
+    consumer_key        :  'ZoIW8ZZvg7DnHtGMwpXDDfZwJ',
+    consumer_secret     :  'fazRHCF8hpFm0tFGkJNBc82GKWbAhypXG7gIKnx3jUHcDx8rwT',
+    access_token        :  '894222030513602561-pJkisMyQPectFhJZ5DbyvtdIsj6kJb5',
+    access_token_secret :  '1fdKqBlrU5YhLGv1Q9cgrcbMeRY1uMjXQVaY5hbfc9g3Z',
+  },
+
+  pusher: {
+    appId: '379485',
+    key: '2e871a8885421241fffd',
+    secret: '35bb1be91bf2f268b9c9',
+    cluster: 'eu',
+    encrypted: true
+  },
+
+  hashtagsToTrack: ['#nodejs', '#swift', '#ios', '#programming'],
+
+  channel: 'hashtags',
+
+  event: 'new_tweet',
+}
